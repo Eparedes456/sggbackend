ff3db361c02bd146b7c611b0e63f2d1c7d11d084
Eparedes456 <erickparedes456@gmail.com>
2021-08-31 23:39:44 -0500

    upload new project

---
+++ src/main/java/com/example/demo/SggdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SggdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SggdemoApplication.class, args);
+	}
+
+}
