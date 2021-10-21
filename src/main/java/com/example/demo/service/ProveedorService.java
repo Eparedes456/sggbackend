148b86c92813cbecad3333849100eef50cddd2ca
Eparedes456 <erickparedes456@gmail.com>
2021-10-21 00:03:01 -0500

    upload

--- src/main/java/com/example/demo/service/ProveedorService.java
+++ src/main/java/com/example/demo/service/ProveedorService.java
@@ -3,9 +3,12 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.example.demo.models.Proveedor;
 import com.example.demo.repository.ProveedorRepository;
 
+@Service
 public class ProveedorService {
 
 	@Autowired
