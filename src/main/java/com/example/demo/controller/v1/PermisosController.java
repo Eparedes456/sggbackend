0c0226038a02b0bb78acca56cda29c2a12ee9664
Eparedes456 <erickparedes456@gmail.com>
2021-11-02 17:55:37 -0500

    upload new features

---
+++ src/main/java/com/example/demo/controller/v1/PermisosController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller.v1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.demo.service.PermisosService;
+
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping(value = "/api/v1/permisos", produces = "Application/json")
+@CrossOrigin(origins = "*")
+public class PermisosController {
+
+	@Autowired
+	private PermisosService permisosService;
+	
+	
+	@ApiOperation(value = "LISTAR TODOS LOS PERMISOS")
+	@GetMapping
+	public ResponseEntity<?> findAllPermisos() {
+		return new ResponseEntity<>(permisosService.findAll(),HttpStatus.OK);
+	}
+	
+	
+	
+	
+}
